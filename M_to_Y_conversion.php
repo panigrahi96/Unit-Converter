f19cdcca1544c73b3fca693d5f5422d0d3f95f59
Sanjukta Chowdhury <donalovesanime@gmail.com>
2019-07-11 15:26:29 +0530

    This is meter to yard conversation page

---
+++ M_to_Y_conversion.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="size.css">
+
+<title>Meter to Yard and Yard to Meter Unit Converter</title>
+
+</head>
+<body>
+<h1>Meter To Yard and Yard To Meter Converter</h1>
+
+<form action="" method="POST">
+Meter: <input type="text" name="unitmeter" value="<?php 
+if(isset($_POST['Y_to_M']))
+{
+	    $meter = $_POST['unityard'];
+		$yd = $meter * 1.0936;
+		echo $yd;
+		
+		
+}
+?>"></br></br></br>
+Yard: <input type="text" name="unityard" value="<?php 
+
+if(isset($_POST['M_to_Y']))
+{
+	    $yard = $_POST['unitmeter'];
+		$m = $yard/1.0936;
+		echo $m;
+}
+?>"></br></br>
+<input type="submit" value="Meter To Yard" name="M_to_Y"></br></br>
+<input type="submit" value="Yard To Meter" name="Y_to_M">
+
+</form>
+</body>
+</html>
